fe0eec2bec6a0559e9dd9b6df850362b4c59d036
Jon Craton <jncraton@gmail.com>
2016-06-23 16:57:52 -0400

    Obsessive hand minification

--- array.vector.js
+++ array.vector.js
@@ -1,18 +1,14 @@
-((A,M) => {
-  A.distSq = function (b) {
-    return this.reduce((p,c,i,a) => p + M.pow(a[i]-b[i],2), 0)
-  }
-  A.dist = function (b) { return M.sqrt(this.distSq(b)) }
+((A,M,F) => {
+  A.distSq = F('this.reduce((p,c,i,a) => p + Math.pow(a[i]-b[i],2), 0)')
+  A.dist = F('Math.sqrt(this.distSq(b))')
 
-  A.magSq = function () {
-    return this.reduce((p,c) => p + (c*c), 0)
-  }
-  A.mag = function () { return M.sqrt(this.magSq()) }
+  A.magSq = F('this.reduce((p,c) => p + (c*c), 0)')
+  A.mag = F('Math.sqrt(this.magSq())')
 
-  A.add = function(b) { return this.map((c,i,a) => a[i] + b[i]) }
-  A.sub = function(b) { return this.map((c,i,a) => a[i] - b[i]) }
-  A.mul = function(b) { return this.map((c,i,a) => a[i] * b) }
-  A.div = function(b) { return this.map((c,i,a) => a[i] / b) }
+  A.add = F('this.map((c,i,a) => a[i] + b[i])')
+  A.sub = F('this.map((c,i,a) => a[i] - b[i])')
+  A.mul = F('this.map((c,i,a) => a[i] * b)')
+  A.div = F('this.map((c,i,a) => a[i] / b)')
 
   'zyxw'.split('').forEach((c,i) => {
     A[c] = function(v) {
@@ -27,4 +23,4 @@
       return this.map((v) => M[f](v,p))
     }
   })
-})(Array.prototype, Math)
+})(Array.prototype, Math, (f)=>Function('b','return '+f))
