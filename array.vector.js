74a1c9678b498f3944cbf3014d96c46bd9fe5152
Jon Craton <jncraton@gmail.com>
2016-06-22 21:23:39 -0400

    Refactored to use implicit returns

--- array.vector.js
+++ array.vector.js
@@ -1,24 +1,16 @@
 Array.prototype.distSq = function (b) {
-  return this.reduce((prev,cur,i) => {
-    return prev + (this[i]-b[i])*(this[i]-b[i])
-  }, 0)
+  return this.reduce((prev,cur,i) => prev + (this[i]-b[i])*(this[i]-b[i]), 0)
 }
 
 Array.prototype.magSq = function () {
-  return this.reduce((prev,cur) => {
-    return prev + (cur*cur)
-  }, 0)
+  return this.reduce((prev,cur) => prev + (cur*cur), 0)
 }
 
-Array.prototype.add = function(b) {
-  return this.map((cur,i) => {
-    return (this[i] + b[i])
-  })
-}
+Array.prototype.add = function(b) { return this.map((cur,i) => this[i] + b[i]) }
 
 Object.keys(Array.prototype).forEach((method) => {
   if (method.endsWith('Sq')) {
-    Array.prototype[method.slice(0, -2)] = function (b) { 
+    Array.prototype[method.slice(0, -2)] = function (b) {
       return Math.sqrt(this[method](b))
     }
   }
