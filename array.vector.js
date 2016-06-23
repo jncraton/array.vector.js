e2c0f4b9c66e73b3a131c0542077843e43ea8f48
Jon Craton <jncraton@gmail.com>
2016-06-22 21:24:26 -0400

    Added sub mehtod

--- array.vector.js
+++ array.vector.js
@@ -7,6 +7,7 @@ Array.prototype.magSq = function () {
 }
 
 Array.prototype.add = function(b) { return this.map((cur,i) => this[i] + b[i]) }
+Array.prototype.sub = function(b) { return this.map((cur,i) => this[i] - b[i]) }
 
 Object.keys(Array.prototype).forEach((method) => {
   if (method.endsWith('Sq')) {
