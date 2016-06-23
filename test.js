40222a66aa1733ccfa6cec7aa32be84d001b53fc
Jon Craton <jncraton@gmail.com>
2016-06-23 16:11:02 -0400

    Also run tests on minified code if present

--- test.js
+++ test.js
@@ -1,26 +1,31 @@
 var fs = require('fs')
 
-eval(String(fs.readFileSync('array.vector.js')))
-
-failures = 0
-
 tests = String(fs.readFileSync('tests.tsv')).split('\n').splice(1)
 
-tests.forEach(function (test, i) {
-  test = test.split('\t')
-  
-  test[1] = JSON.parse(`[${test[1]}]`)
-  test[2] = JSON.parse(`[${test[2]}]`)
-  test[3] = JSON.parse(test[3])
+;['array.vector.js','array.vector.min.js'].forEach(function (file) {
+  failures = 0
 
-  var result = test[1][test[0]].apply(test[1], test[2])
-  
-  if (JSON.stringify(result) != JSON.stringify(test[3])) {
-    console.log(`Test ${i+1} Failed (${test[0]} got ${result}, expect ${test[3]})`)
-    failures += 1
+  try {
+    eval(String(fs.readFileSync(file)))
+
+    tests.forEach(function (test, i) {
+      test = test.split('\t')
+      
+      test[1] = JSON.parse(`[${test[1]}]`)
+      test[2] = JSON.parse(`[${test[2]}]`)
+      test[3] = JSON.parse(test[3])
+
+      var result = test[1][test[0]].apply(test[1], test[2])
+      
+      if (JSON.stringify(result) != JSON.stringify(test[3])) {
+        console.log(`Test ${i+1} Failed (${test[0]} got ${result}, expect ${test[3]})`)
+        failures += 1
+      }
+    })
+    console.log(`${file} ${tests.length - failures}/${tests.length} tests passed`)
+  } catch(e) {
+    console.log(file + " failed to load")
   }
 })
-
-console.log(`${tests.length - failures}/${tests.length} tests passed`)
 
 process.exit(failures)
