a7b654642578c9d408e69599099e0ff52adda3a5
Jon Craton <jncraton@gmail.com>
2016-06-22 21:05:55 -0400

    Shorter method names

--- array.vector.js
+++ array.vector.js
@@ -1,10 +1,10 @@
-Array.prototype.distanceSq = function (b) {
+Array.prototype.distSq = function (b) {
   return this.reduce((prev,cur,i) => {
     return prev + (this[i]-b[i])*(this[i]-b[i])
   }, 0)
 }
 
-Array.prototype.magnitudeSq = function () {
+Array.prototype.magSq = function () {
   return this.reduce((prev,cur) => {
     return prev + (cur*cur)
   }, 0)
