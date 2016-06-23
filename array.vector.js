806392acd0f31bdac952400ee23ada66efc3f43e
Jon Craton <jncraton@gmail.com>
2016-06-23 15:29:47 -0400

    Added include Math as a local

--- array.vector.js
+++ array.vector.js
@@ -1,29 +1,29 @@
-(($) => {
-  $.distSq = function (b) {
-    return this.reduce((p,c,i) => p + Math.pow((this[i]-b[i]),2), 0)
+((A,M) => {
+  A.distSq = function (b) {
+    return this.reduce((p,c,i) => p + M.pow((this[i]-b[i]),2), 0)
   }
-  $.dist = function (b) { return Math.sqrt(this.distSq(b)) }
+  A.dist = function (b) { return M.sqrt(this.distSq(b)) }
 
-  $.magSq = function () {
+  A.magSq = function () {
     return this.reduce((p,c) => p + (c*c), 0)
   }
-  $.mag = function () { return Math.sqrt(this.magSq()) }
+  A.mag = function () { return M.sqrt(this.magSq()) }
 
-  $.add = function(b) { return this.map((c,i,a) => a[i] + b[i]) }
-  $.sub = function(b) { return this.map((c,i,a) => a[i] - b[i]) }
-  $.mul = function(b) { return this.map((c,i,a) => a[i] * b) }
-  $.div = function(b) { return this.map((c,i,a) => a[i] / b) }
+  A.add = function(b) { return this.map((c,i,a) => a[i] + b[i]) }
+  A.sub = function(b) { return this.map((c,i,a) => a[i] - b[i]) }
+  A.mul = function(b) { return this.map((c,i,a) => a[i] * b) }
+  A.div = function(b) { return this.map((c,i,a) => a[i] / b) }
 
   'zyxw'.split('').forEach((c,i) => {
-    $[c] = function(v) {
+    A[c] = function(v) {
       if (v) this[this.length - 1 - i] = v
       return this[this.length - 1 - i]
     }
   })
 
   'abs ceil floor max min round'.split(' ').forEach((f,i) => {
-    $[f] = function(p) {
-      return this.map((v) => v = Math[f](v,p))
+    A[f] = function(p) {
+      return this.map((v) => v = M[f](v,p))
     }
   })
-})(Array.prototype)
+})(Array.prototype, Math)
