4fbc212e38344300604eff7a4aae823c302be5e1
Jon Craton <jncraton@gmail.com>
2016-06-23 14:24:49 -0400

    Cut down byte count

--- array.vector.js
+++ array.vector.js
@@ -1,33 +1,33 @@
 Array.prototype.distSq = function (b) {
-  return this.reduce((prev,cur,i) => prev + (this[i]-b[i])*(this[i]-b[i]), 0)
+  return this.reduce((p,c,i) => p + (this[i]-b[i])*(this[i]-b[i]), 0)
 }
 
 Array.prototype.magSq = function () {
-  return this.reduce((prev,cur) => prev + (cur*cur), 0)
+  return this.reduce((p,c) => p + (c*c), 0)
 }
 
-Array.prototype.add = function(b) { return this.map((cur,i) => this[i] + b[i]) }
-Array.prototype.sub = function(b) { return this.map((cur,i) => this[i] - b[i]) }
-Array.prototype.mul = function(b) { return this.map((cur,i) => this[i] * b) }
-Array.prototype.div = function(b) { return this.map((cur,i) => this[i] / b) }
+Array.prototype.add = function(b) { return this.map((c,i) => this[i] + b[i]) }
+Array.prototype.sub = function(b) { return this.map((c,i) => this[i] - b[i]) }
+Array.prototype.mul = function(b) { return this.map((c,i) => this[i] * b) }
+Array.prototype.div = function(b) { return this.map((c,i) => this[i] / b) }
 
-;['z','y','x','w'].forEach((dim,i) => {
-  Array.prototype[dim] = function(val) {
-    if (val) this[[this.length - 1 - i]] = val
-    return this[[this.length - 1 - i]]
+'zyxw'.split('').forEach((c,i) => {
+  Array.prototype[c] = function(v) {
+    if (v) this[this.length - 1 - i] = v
+    return this[this.length - 1 - i]
   }
 })
 
-;['abs','ceil','floor','max','min','round'].forEach((fn,i) => {
-  Array.prototype[fn] = function(param) {
-    return this.map((val) => val = Math[fn](val,param))
+;['abs','ceil','floor','max','min','round'].forEach((f,i) => {
+  Array.prototype[f] = function(p) {
+    return this.map((v) => v = Math[f](v,p))
   }
 })
 
-Object.keys(Array.prototype).forEach((method) => {
-  if (method.endsWith('Sq')) {
-    Array.prototype[method.slice(0, -2)] = function (b) {
-      return Math.sqrt(this[method](b))
+Object.keys(Array.prototype).forEach((f) => {
+  if (f.endsWith('Sq')) {
+    Array.prototype[f.slice(0, -2)] = function (b) {
+      return Math.sqrt(this[f](b))
     }
   }
 })
