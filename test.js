24999c927986f79a8b692a11e50c75098d2219bf
Jon Craton <jncraton@gmail.com>
2016-06-22 23:30:42 -0400

    Removed extra parens on tests

--- test.js
+++ test.js
@@ -7,13 +7,11 @@ failures = 0
 tests = String(fs.readFileSync('tests.tsv')).split('\n').splice(1)
 
 tests.forEach(function (test, i) {
-  test = test.split('\t').map((col,i) => {
-    if (i > 0) {
-      return JSON.parse(col)
-    } else {
-      return col
-    }
-  })
+  test = test.split('\t')
+  
+  test[1] = JSON.parse(test[1])
+  test[2] = JSON.parse(`[${test[2]}]`)
+  test[3] = JSON.parse(test[3])
 
   var result = test[1][test[0]].apply(test[1], test[2])
   
