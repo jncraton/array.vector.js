e177d3b80b7760b3740085853e109b44916fd369
Jon Craton <jncraton@gmail.com>
2016-06-24 00:07:20 -0400

    More trickery to save space

--- array.vector.js
+++ array.vector.js
@@ -1,14 +1,14 @@
-((A,F) => {
-  A.distSq = F('reduce((p,c,i,a) => p + Math.pow(a[i]-b[i],2), 0)')
-  A.dist = F('Math.sqrt(distSq(b))')
+((A,F,M,R) => {
+  A.distSq = F(R + 'p + Math.pow(a[i]-b[i],2), 0')
+  A.dist = F('Math.sqrt(distSq(b)')
 
-  A.magSq = F('reduce((p,c) => p + (c*c), 0)')
-  A.mag = F('Math.sqrt(magSq())')
+  A.magSq = F(R + 'p + (c*c), 0')
+  A.mag = F('Math.sqrt(magSq()')
 
-  A.add = F('map((c,i,a) => a[i] + b[i])')
-  A.sub = F('map((c,i,a) => a[i] - b[i])')
-  A.mul = F('map((c,i,a) => a[i] * b)')
-  A.div = F('map((c,i,a) => a[i] / b)')
+  A.add = F(M + 'a[i] + b[i]')
+  A.sub = F(M + 'a[i] - b[i]')
+  A.mul = F(M + 'a[i] * b')
+  A.div = F(M + 'a[i] / b')
 
   'zyxw'.split('').forEach((c,i) => {
     A[c] = function(v) {
@@ -18,6 +18,6 @@
     }
   })
 
-  for(f of 'abs ceil floor max min round'.split(' ')) A[f] = F(`map((v) => Math.${f}(v,b))`)
+  for(f of 'abs ceil floor max min round'.split(' ')) A[f] = F(M + `Math.${f}(c,b)`)
 
-})(Array.prototype, (f)=>Function('b',`with(this){return ${f}}`))
+})(Array.prototype, (f)=>Function('b',`with(this){return ${f})}`), 'map((c,i,a)=>', 'reduce((p,c,i,a)=>')
