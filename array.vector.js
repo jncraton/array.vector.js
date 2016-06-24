57dcce94d298c6d64bca87a57e0790c31e799079
Jon Craton <jncraton@gmail.com>
2016-06-24 14:12:02 -0400

    Added dot product

--- array.vector.js
+++ array.vector.js
@@ -15,6 +15,11 @@ Array.prototype.sub = function (b) { return this.map((cur,i,a) => a[i] - b[i]) }
 Array.prototype.mul = function (b) { return this.map((cur,i,a) => a[i] * b) }
 Array.prototype.div = function (b) { return this.map((ccur,i,a) => a[i] / b) }
 
+// Σaₐbₐ
+Array.prototype.dot = function (b) {
+  return this.reduce((sum,cur,i,a) => sum + a[i]*b[i], 0)
+}
+
 Array.prototype.cross = function (b) {
   return [
     this[1]*b[2] - this[2]*b[1],
