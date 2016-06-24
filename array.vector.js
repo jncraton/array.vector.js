762d198f1e21c935b9be1de3f1a57fa149f1306e
Jon Craton <jncraton@gmail.com>
2016-06-23 23:36:06 -0400

    Shorter function syntax with 'this' local

--- array.vector.js
+++ array.vector.js
@@ -1,14 +1,14 @@
-((A,M,F) => {
-  A.distSq = F('this.reduce((p,c,i,a) => p + Math.pow(a[i]-b[i],2), 0)')
-  A.dist = F('Math.sqrt(this.distSq(b))')
+((A,F) => {
+  A.distSq = F('reduce((p,c,i,a) => p + Math.pow(a[i]-b[i],2), 0)')
+  A.dist = F('Math.sqrt(distSq(b))')
 
-  A.magSq = F('this.reduce((p,c) => p + (c*c), 0)')
-  A.mag = F('Math.sqrt(this.magSq())')
+  A.magSq = F('reduce((p,c) => p + (c*c), 0)')
+  A.mag = F('Math.sqrt(magSq())')
 
-  A.add = F('this.map((c,i,a) => a[i] + b[i])')
-  A.sub = F('this.map((c,i,a) => a[i] - b[i])')
-  A.mul = F('this.map((c,i,a) => a[i] * b)')
-  A.div = F('this.map((c,i,a) => a[i] / b)')
+  A.add = F('map((c,i,a) => a[i] + b[i])')
+  A.sub = F('map((c,i,a) => a[i] - b[i])')
+  A.mul = F('map((c,i,a) => a[i] * b)')
+  A.div = F('map((c,i,a) => a[i] / b)')
 
   'zyxw'.split('').forEach((c,i) => {
     A[c] = function(v) {
@@ -18,9 +18,7 @@
     }
   })
 
-  'abs ceil floor max min round'.split(' ').forEach((f,i) => {
-    A[f] = function(p) {
-      return this.map((v) => M[f](v,p))
-    }
-  })
-})(Array.prototype, Math, (f)=>Function('b','return '+f))
+  'abs ceil floor max min round'.split(' ').forEach((f,i) =>
+    A[f] = F(`map((v) => Math.${f}(v,b))`)
+  )
+})(Array.prototype, (f)=>Function('b',`with(this){return ${f}}`))
