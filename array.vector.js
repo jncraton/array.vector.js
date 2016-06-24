26efaa14d028409334807802747e1f9ee2c5275a
Jon Craton <jncraton@gmail.com>
2016-06-24 13:33:49 -0400

    More symantic variable names

--- array.vector.js
+++ array.vector.js
@@ -1,33 +1,33 @@
 Array.prototype.distSq = function (b) {
-  return this.reduce((p,c,i,a) => p + Math.pow(a[i]-b[i],2), 0)
+  return this.reduce((sum,cur,i,a) => sum + Math.pow(a[i]-b[i],2), 0)
 }
 
 Array.prototype.dist = function (b) { return Math.sqrt(this.distSq(b)) }
 
 Array.prototype.magSq = function (b) {
-  return this.reduce((p,c,i,a) => p + (c*c), 0)
+  return this.reduce((sum,cur,i,a) => sum + (cur*cur), 0)
 }
 
 Array.prototype.mag = function (b) { return Math.sqrt(this.magSq(b)) }
 
-Array.prototype.add = function (b) { return this.map((c,i,a) => a[i] + b[i]) }
-Array.prototype.sub = function (b) { return this.map((c,i,a) => a[i] - b[i]) }
-Array.prototype.mul = function (b) { return this.map((c,i,a) => a[i] * b) }
-Array.prototype.div = function (b) { return this.map((c,i,a) => a[i] / b) }
+Array.prototype.add = function (b) { return this.map((cir,i,a) => a[i] + b[i]) }
+Array.prototype.sub = function (b) { return this.map((cur,i,a) => a[i] - b[i]) }
+Array.prototype.mul = function (b) { return this.map((cur,i,a) => a[i] * b) }
+Array.prototype.div = function (b) { return this.map((ccur,i,a) => a[i] / b) }
 
-Array.prototype.x = function (v) { 
-  if (v) this[0] = v
+Array.prototype.x = function (newVal) {
+  if (newVal) this[0] = newVal
   return this[0]
 }
-Array.prototype.y = function (v) { 
-  if (v) this[1] = v
+Array.prototype.y = function (newVal) {
+  if (newVal) this[1] = newVal
   return this[1]
 }
-Array.prototype.z = function (v) { 
-  if (v) this[2] = v
+Array.prototype.z = function (newVal) {
+  if (newVal) this[2] = newVal
   return this[2]
 }
 
-;['abs','ceil','floor','max','min','round'].forEach(function (f) {
-  Array.prototype[f] = function (b) { return this.map((c,i,a) => Math[f](c,b)) }
+;['abs','ceil','floor','max','min','round'].forEach(function (fn) {
+  Array.prototype[fn] = function (b) { return this.map((cur,i,a) => Math[fn](cur,b)) }
 })
